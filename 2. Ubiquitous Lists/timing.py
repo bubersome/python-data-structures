baa1a78b45482976b681f37abb32c70ff2432bc9
bubersome <bubersome@gmail.com>
2022-04-07 19:39:22 -0500

    0407

--- 2. Ubiquitous Lists/timing.py
+++ 2. Ubiquitous Lists/timing.py
@@ -25,3 +25,16 @@ N	CircBuf  	NaiveBuf
 8192	1.429783	4.392674
 16384	1.434091	7.873656
 """
+
+"""
+/opt/homebrew/Caskroom/miniconda/base/envs/python-data-structures/bin/python "/Users/32uni/PycharmProjects/python-data-structures/2. Ubiquitous Lists/timing.py"
+N	CircBuf  	NaiveBuf
+1024	0.345675	0.287803
+2048	0.348456	0.428538
+4096	0.349790	0.689333
+8192	0.349623	1.222661
+16384	0.351786	2.712583
+
+Process finished with exit code 0
+
+"""
